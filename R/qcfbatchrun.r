0c7e3e05eb40999af4dd862cb277d07e1f7c23c4
YufanC <whchenyufan@yahoo.com>
2023-04-17 18:15:58 +0800

    Work for non-interactive run

--- R/qcfbatchrun.r
+++ R/qcfbatchrun.r
@@ -20,33 +20,55 @@ qc_batchrun <- function(files, path = qc){
   
   ### Delete temp1.txt if it exists
   if (file.exists(file.path(envsetup::write_path(path), "temp1.txt"))) {
-    unlink(envsetup::read_path(qc, "temp1.txt"))
+    unlink(envsetup::read_path(path, "temp1.txt"))
   }
   
   sapply(files, source)
+  temp_result <- file.path(envsetup::read_path(path, "temp1.txt"))
   
-  ### Output comparison result to viewer
-  # Create temp directory and files
-  tempDir <- tempfile(); dir.create(tempDir)
-  temp_file <- file.path(tempDir, "temp.html")
-  temp_result <- file.path(envsetup::write_path(qc), "temp1.txt")
-  
-  # diverts output to a temp file
-  sink(temp_file, append = TRUE)
-  cat("<h2>Comparison Results</h2>")
-  cat("<table border='1'>")
-  cat("<tr>")
-  cat("<th>Table id</th>")
-  cat("<th>Results match</th>")
-  cat("</tr>")
-  cat(readLines(temp_result, warn = F))
-  cat("</table>")
-  
-  # vie file in Viewer window
-  viewer <- getOption("viewer")
-  rstudioapi::viewer(temp_file)
-  # diverts output back to console
-  sink()
+  if (interactive()){
+    ### Output comparison result to viewer
+    # Create temp directory and files
+    tempDir <- tempfile(); dir.create(tempDir)
+    temp_file <- file.path(tempDir, "temp.html")
+    
+    # diverts output to a temp file
+    sink(temp_file, append = TRUE)
+    cat("<h2>Comparison Results</h2>")
+    cat("<table border='1'>")
+    cat("<tr>")
+    cat("<th>Table id</th>")
+    cat("<th>Results match</th>")
+    cat("</tr>")
+    cat(readLines(temp_result, warn = F))
+    cat("</table>")
+    
+    # vie file in Viewer window
+    viewer <- getOption("viewer")
+    rstudioapi::viewer(temp_file)
+    
+    # diverts output back to console
+    sink()
+    
+  } else {
+    
+    ### Delete html_output.txt if it exists
+    if (file.exists(file.path(envsetup::write_path(path), "html_output.txt"))) {
+      unlink(envsetup::read_path(path, "html_output.txt"))
+    }
+    
+    ### Create compare_results.html when run not interactively
+    cat("<h2>Comparison Results</h2>", "<table border='1'>", "<tr>", 
+        "<th>Table id</th>", "<th>Results match</th>", "</tr>", 
+        paste(readLines(temp_result, warn = F), collapse = ""), "</table>",
+        file = file.path(write_path(path), "html_output.txt"), sep = "")
+    
+    html_output <- file.path(envsetup::read_path(path, "html_output.txt"))
+    
+    knitr::knit(html_output, output = "compare_results.html")
+    
+    unlink(html_output)
+  }
   
   unlink(temp_result)
   
