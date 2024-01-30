be507e370e20c57556cd15517f60e2c4376da26d
YufanC <whchenyufan@yahoo.com>
2024-01-30 10:55:19 +0800

    Bug fixing

--- R/qc_cntpct_byrowvar.r
+++ R/qc_cntpct_byrowvar.r
@@ -1,5 +1,36 @@
-qc_cntpct_byrowvar1 <- function(input, colvar = "TRT01P", rowvar = c("AEBODSYS", "AEDECOD"), row_text = "Subjects with 1 or more AEs", N_row, col_order = NULL, subset = NULL){
+#' Create Count and Percentage Table by Row Variables
+#'
+#' compute count and percentage of SOC(class)/PT by column variable with an ordering
+#' @param input input dataframe 
+#' @param colvar column variable 
+#' @param rowvar row variable (can be single variable such as "AEDECOD" or multiple variable such as c("AEBODSYS", "AEDECOD"))
+#' @param row_text row text 
+#' @param N_row dataframe with N 
+#' @param col_order ordering column name. e.g. "n_5". Default is NULL and PTs will be ordered by row-wise sum 
+#' @param subset subset criteria. Default = NULL
+#' @return a dataframe containing count and percentage of SOC/PT by colvar
+#' @examples 
+#' aedecod <- sample(paste0("PT", 1:3), 10, replace = TRUE)
+#' 
+#' adae <- data.frame(
+#'   USUBJID = 1:10,
+#'   TRT01P = factor(sample(c("A", "B", "C"), 10, replace = TRUE)),
+#'   SEX = as.factor(sample(c("Female", "Male"), 10, replace = TRUE)),
+#'   AEBODSYS = ifelse(aedecod == "PT1", "SOC1", "SOC2"),
+#'   AEDECOD = aedecod)
+#' 
+#' ### Create analysis row first
+#' first_row <- qc_cntrow1(input = adae, colvar = "TRT01P", row_text = "Analysis set: Safety")
+#' 
+#' tab1 <- qc_cntpct_byrowvar(input = adae, colvar = "TRT01P", rowvar = c("AEBODSYS", "AEDECOD"),
+#'            row_text = "Subjects with 1 or more AEs", N_row = first_row$N_row)
+#' tab1
+#' @export
+qc_cntpct_byrowvar <- function(input, colvar = "TRT01P", rowvar = c("AEBODSYS", "AEDECOD"), row_text = "Subjects with 1 or more AEs", N_row, col_order = NULL, subset = NULL){
   
+  assertthat::assert_that(not_empty(input))
+  assertthat::assert_that(assertthat::has_name(input, c(colvar, rowvar)))
+  assertthat::assert_that(is.factor(input[[colvar]]))
   ### first N row
   row1 <- input %>%
     group_by(.data[[colvar]], .drop = F) %>% 
